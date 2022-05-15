7eef4cc7ffaac162c065ceda94124a446004372f
owentr1369 <tamvaa1306@gmail.com>
2022-05-15 11:30:18 +0700

    Integrate User Sign-Up API

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -6,17 +6,39 @@
     />
     <h1>Sign Up</h1>
     <div class="register">
-      <input type="text" placeholder="Enter Name" />
-      <input type="email" placeholder="Enter email" />
-      <input type="password" placeholder="Enter password" />
-      <button>Sign Up</button>
+      <input type="text" v-model="name" placeholder="Enter Name" />
+      <input type="email" v-model="email" placeholder="Enter email" />
+      <input type="password" v-model="password" placeholder="Enter password" />
+      <button v-on:click="signUp">Sign Up</button>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      let result = await axios.post("http://localhost:3000/users", {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      });
+      console.log(result);
+      if (result.status == 201) {
+        alert("sign up done");
+        localStorage.setItem("user-info", JSON.stringify(result.data));
+      }
+    },
+  },
 };
 </script>
 
@@ -51,5 +73,7 @@ img {
   cursor: pointer;
   margin-right: auto;
   margin-left: auto;
+  font-weight: 700;
+  font-size: 1rem;
 }
 </style>
